26d3935e6ee6922619bb95ad83488325338e33af
Nassary <89366802+Nassary@users.noreply.github.com>
2023-04-28 20:16:32 +0200

    Add files via upload

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "The Godfather",
+    artist: "Michael Corleone",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Ameer Al Zalam",
+    artist: "Adel Emam",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Aalamy",
+    artist: "Youssef El Sherif",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Abo Ali",
+    artist: "Karim Abdel Aziz",
+    img: "img4",
+    audio: "music4"
+  }
+]
